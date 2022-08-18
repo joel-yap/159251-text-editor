62634fe900e6016f076b29cc7db551b612b4d87c
Joel Yap <jy202019@gmail.com>
2022-08-18 22:16:49 +1200

    Added Open file function

--- 159251assignment1/src/main/java/app/StartWindow.java
+++ 159251assignment1/src/main/java/app/StartWindow.java
@@ -3,6 +3,12 @@ package app;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
@@ -12,6 +18,11 @@ public class StartWindow {
 	private JMenu fileMenu;
 	private JMenuItem newItem;
 	private JEditorPane editorPane;
+	private JMenuItem openItem;
+	private JFileChooser chooser;
+	private Container container;
+	private int dialogValue;
+	private String filename;
 
 	public StartWindow() {
 // Frame
@@ -55,6 +66,37 @@ public class StartWindow {
 		frame.setContentPane(editorPane);
 		frame.setVisible(true);
 
+// File menu item: Open File
+		openItem = new JMenuItem("Open");
+		container = frame.getContentPane();
+		chooser = new JFileChooser();
+		//container = null;
+		dialogValue = 0;
+		openItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dialogValue = chooser.showOpenDialog(container);
+				if (dialogValue == JFileChooser.APPROVE_OPTION) {
+					String filePath = chooser.getSelectedFile().getPath();
+					try {
+						FileInputStream fr = new FileInputStream(filePath);
+						InputStreamReader isr = new InputStreamReader(fr, "UTF-8");
+						BufferedReader reader = new BufferedReader(isr);
+						StringBuffer buffer = new StringBuffer();
+						String line = null;
+						while ((line = reader.readLine()) != null) {
+							buffer.append(line);
+							buffer.append("\n");
+						}
+						reader.close();
+						editorPane.setText(buffer.toString());
+					} catch (IOException e2) {
+						e2.printStackTrace();
+					}
+				}
+			}
+		});
+		fileMenu.add(openItem);
 	}
+
 
 }
