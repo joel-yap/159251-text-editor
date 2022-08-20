23ea01684afd84cc4a3e3519730dff30c5aff710
Joel Yap <jy202019@gmail.com>
2022-08-20 16:19:39 +1200

    Added Save Feature

--- 159251assignment1/src/main/java/app/StartWindow.java
+++ 159251assignment1/src/main/java/app/StartWindow.java
@@ -4,8 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -17,6 +19,7 @@ public class StartWindow {
 	private JMenuBar mainMenuBar;
 	private JMenu fileMenu;
 	private JMenuItem newItem;
+	private JMenuItem saveItem;
 	private JEditorPane editorPane;
 	private JMenuItem openItem;
 	private JFileChooser chooser;
@@ -95,6 +98,42 @@ public class StartWindow {
 			}
 		});
 		fileMenu.add(openItem);
+		
+// File menu item: Save File
+		saveItem = new JMenuItem("Save");
+		container = frame.getContentPane();
+		chooser = new JFileChooser();
+		//container = null;
+		dialogValue = 0;
+		saveItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dialogValue = chooser.showSaveDialog(container);
+				if (dialogValue == JFileChooser.APPROVE_OPTION) {
+					String filePath = chooser.getSelectedFile().getPath();
+					try {
+						File myObj = new File(filePath);
+						if (myObj.createNewFile()) {
+							System.out.println("File created: " + myObj.getName());
+						} else {
+							System.out.println("File already exists.");
+						}
+					} catch (IOException e2) {
+						System.out.println("An error occurred.");
+						e2.printStackTrace();
+					}
+					try {
+						FileWriter myWriter = new FileWriter(filePath);
+						myWriter.write(editorPane.getText());
+						myWriter.close();
+						System.out.println("Successfully wrote to file.");
+					} catch (IOException e3) {
+						System.out.println("An error occurred.");
+						e3.printStackTrace();
+					}
+				}
+			}
+		});
+		fileMenu.add(saveItem);
 	}
 
 
