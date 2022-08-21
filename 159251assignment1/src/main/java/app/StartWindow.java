5ee830f16276d8e3b59a0a715647dda56271878f
André Vrey <21009282@massey.ac.nz>
2022-08-22 03:03:30 +1200

    Added Search

--- 159251assignment1/src/main/java/app/StartWindow.java
+++ 159251assignment1/src/main/java/app/StartWindow.java
@@ -1,6 +1,8 @@
 package app;
 
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.BufferedReader;
@@ -18,6 +20,7 @@ public class StartWindow {
 	private JFrame frame;
 	private JMenuBar mainMenuBar;
 	private JMenu fileMenu;
+	private JMenu searchMenu;
 	private JMenuItem newItem;
 	private JMenuItem saveItem;
 	private JEditorPane editorPane;
@@ -29,26 +32,48 @@ public class StartWindow {
 	private JScrollPane scrPane;
 
 	public StartWindow() {
-// Frame
+// Create main window
 		frame = new JFrame("Text Editor");
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setBounds(200,170,500,500);
 
-// Menu bar
+// Create main menu
 		mainMenuBar = new JMenuBar();
 		frame.setJMenuBar(mainMenuBar);
 
-// Menu bar item: File
+// Create main menu item: File
 		fileMenu = new JMenu("File");
 		mainMenuBar.add(fileMenu);
 
-// File menu item: New File
+// Create main menu item: Search
+		searchMenu = new JMenu("Search");
+		searchMenu.setPopupMenuVisible(false);
+		searchMenu.addMenuListener(new MenuListener() {
+			@Override
+			public void menuSelected(MenuEvent e) {
+				searchFile();
+				editorPane.requestFocusInWindow();
+			}
+
+			@Override
+			public void menuDeselected(MenuEvent e) {
+
+			}
+
+			@Override
+			public void menuCanceled(MenuEvent e) {
+
+			}
+		});
+		mainMenuBar.add(searchMenu);
+
+// Create main menu item: File > New
 		newItem = new JMenuItem(new AbstractAction("New") {
 			public void actionPerformed(ActionEvent e) {
 				int result = JOptionPane.showConfirmDialog(frame, "<html><b>Save changes?</b></html>\n"+ "Unsaved changes will be lost.");
 				switch(result) {
 					case JOptionPane.YES_OPTION:
-						// Save changes
+						saveFile();
 						break;
 					case JOptionPane.NO_OPTION:
 						editorPane.setText("");
@@ -60,15 +85,8 @@ public class StartWindow {
 		});
 		fileMenu.add(newItem);
 
-// Initialize Editor pane
-		editorPane = new JEditorPane();
-		scrPane = new JScrollPane(editorPane, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS );
 
-// Show all elements
-		frame.setContentPane(scrPane);
-		frame.setVisible(true);
-
-// File menu item: Open File
+// Create main menu item: File > Open
 		openItem = new JMenuItem("Open");
 		container = frame.getContentPane();
 		chooser = new JFileChooser();
@@ -76,65 +94,96 @@ public class StartWindow {
 		dialogValue = 0;
 		openItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				dialogValue = chooser.showOpenDialog(container);
-				if (dialogValue == JFileChooser.APPROVE_OPTION) {
-					String filePath = chooser.getSelectedFile().getPath();
-					try {
-						FileInputStream fr = new FileInputStream(filePath);
-						InputStreamReader isr = new InputStreamReader(fr, "UTF-8");
-						BufferedReader reader = new BufferedReader(isr);
-						StringBuffer buffer = new StringBuffer();
-						String line = null;
-						while ((line = reader.readLine()) != null) {
-							buffer.append(line);
-							buffer.append("\n");
-						}
-						reader.close();
-						editorPane.setText(buffer.toString());
-					} catch (IOException e2) {
-						e2.printStackTrace();
-					}
-				}
+				loadFile();
 			}
 		});
 		fileMenu.add(openItem);
-		
-// File menu item: Save File
+
+// Create main menu item: File > Save
 		saveItem = new JMenuItem("Save");
 		container = frame.getContentPane();
 		chooser = new JFileChooser();
-		//container = null;
 		dialogValue = 0;
 		saveItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				dialogValue = chooser.showSaveDialog(container);
-				if (dialogValue == JFileChooser.APPROVE_OPTION) {
-					String filePath = chooser.getSelectedFile().getPath();
-					try {
-						File myObj = new File(filePath);
-						if (myObj.createNewFile()) {
-							System.out.println("File created: " + myObj.getName());
-						} else {
-							System.out.println("File already exists.");
-						}
-					} catch (IOException e2) {
-						System.out.println("An error occurred.");
-						e2.printStackTrace();
-					}
-					try {
-						FileWriter myWriter = new FileWriter(filePath);
-						myWriter.write(editorPane.getText());
-						myWriter.close();
-						System.out.println("Successfully wrote to file.");
-					} catch (IOException e3) {
-						System.out.println("An error occurred.");
-						e3.printStackTrace();
-					}
-				}
+				saveFile();
 			}
 		});
 		fileMenu.add(saveItem);
+
+// Create editor pane
+		editorPane = new JEditorPane();
+		scrPane = new JScrollPane(editorPane, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS );
+
+// Show all main elements
+		frame.setContentPane(scrPane);
+		frame.setVisible(true);
 	}
 
+	void saveFile() {
+		dialogValue = chooser.showSaveDialog(container);
+		if (dialogValue == JFileChooser.APPROVE_OPTION) {
+			String filePath = chooser.getSelectedFile().getPath();
+			try {
+				File myObj = new File(filePath);
+				if (myObj.createNewFile()) {
+					System.out.println("File created: " + myObj.getName());
+				} else {
+					System.out.println("File already exists.");
+				}
+			} catch (IOException e2) {
+				System.out.println("An error occurred.");
+				e2.printStackTrace();
+			}
+			try {
+				FileWriter myWriter = new FileWriter(filePath);
+				myWriter.write(editorPane.getText());
+				myWriter.close();
+				System.out.println("Successfully wrote to file.");
+			} catch (IOException e3) {
+				System.out.println("An error occurred.");
+				e3.printStackTrace();
+			}
+		}
+	}
+
+	void loadFile() {
+		dialogValue = chooser.showOpenDialog(container);
+		if (dialogValue == JFileChooser.APPROVE_OPTION) {
+			String filePath = chooser.getSelectedFile().getPath();
+			try {
+				FileInputStream fr = new FileInputStream(filePath);
+				InputStreamReader isr = new InputStreamReader(fr, "UTF-8");
+				BufferedReader reader = new BufferedReader(isr);
+				StringBuffer buffer = new StringBuffer();
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					buffer.append(line);
+					buffer.append("\n");
+				}
+				reader.close();
+				editorPane.setText(buffer.toString());
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+	void searchFile() {
+		String searchTerm = JOptionPane.showInputDialog(frame, "Enter search term (case sensitive): ");
+		String searchText = editorPane.getText();
+
+		if (searchTerm == null || searchTerm == "" || searchText == "") {
+			return;
+		}
+
+		int i = searchText.indexOf(searchTerm);
+		if (i == -1) {
+			return;
+		} else {
+			editorPane.setCaretPosition(i);
+		}
+		editorPane.select(i, (i + searchTerm.length()));
+	}
 
 }
